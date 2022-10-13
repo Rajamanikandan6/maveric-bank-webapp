16cc8504a76fe487a6936e79d563a64769b3c862
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-10-13 15:58:22 +0530

    adding application signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',loadChildren:() => import('./signup/signup.module')
+  .then(mod=>mod.SignupModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
