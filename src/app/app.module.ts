f6159676194cc25e2a412f8bb99731b6bd2535ef
MAVERICSYSTEMS\supritask <supritask@maveric-systems.com>
2022-10-19 16:41:36 +0530

    added logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthIntercaptor } from './authentication/auth-intercaptor';
 import { AuthGuard } from './authentication/auth.guard';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { AuthGuard } from './authentication/auth.guard';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [AuthGuard,{ provide: HTTP_INTERCEPTORS, useClass: AuthIntercaptor, multi: true }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
