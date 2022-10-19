c4a5d16d1afc2230f095e564a261f1640f3b5cc2
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-10-19 16:43:35 +0530

    bootstrap icon added

--- src/app/authentication/auth-intercaptor.ts
+++ src/app/authentication/auth-intercaptor.ts
@@ -5,7 +5,6 @@ import { Observable } from "rxjs";
 @Injectable()
 export class AuthIntercaptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        localStorage.setItem("token","afsjkfuer67gr78tfg7");
         const token = localStorage.getItem("token");
         if (!token || req.url.search("/auth/login") > 0 || req.url.search("/auth/signup") > 0  ) {
             return next.handle(req);
